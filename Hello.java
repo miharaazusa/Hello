a590c9daed0851c2f678cfb3bc36bee350fe751d
mihara <49392483+miharaazusa@users.noreply.github.com>
2019-04-24 14:50:15 +0900

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,16 @@
+package Hello;
+import java.util.Scanner;
+public class Hello {
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.print(a+b);
+		System.out.print(a-b);
+		System.out.print(a*b);
+		System.out.print(a/b);
+
+	}
+
+}
